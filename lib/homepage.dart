9d771100030bd8fff4bb7471d32ed14a537e3044
virajrungta <virajrungta@gmail.com>
2026-01-16 09:20:23 -0800

    Added navigation bar to the bottom

---
+++ lib/homepage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+Color darkGreen = const Color(0xFF023020);
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics),
+            label: 'Analytics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message_rounded),
+            label: 'Messages',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: darkGreen,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
